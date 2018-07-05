52b73d89c252b4ff9ce090692929c304a6d882db
epishova <34798874+epishova@users.noreply.github.com>
2018-07-05 13:03:33 -0400

    Cleaned code and added comments

--- HTTP/http_ce_produser.py
+++ HTTP/http_ce_produser.py
@@ -10,10 +10,8 @@ YOURPASSWORD =
 
 topic = 'currency_exchange'
 broker = 'ec2-18-209-75-68.compute-1.amazonaws.com:9092, ec2-18-205-142-57.compute-1.amazonaws.com:9092, ec2-50-17-32-144.compute-1.amazonaws.com:9092'
-#broker = 'localhost:9092'
 
 r = requests.get("http://webrates.truefx.com/rates/connect.html?u="+YOURUSERNAME+"&p="+YOURPASSWORD"&q=ozrates&f=csv")
-#print(r.status_code)
 if r.status_code == requests.codes.ok:
     print('HTTP request is OK')
     request_body = "http://webrates.truefx.com/rates/connect.html?id=" + r.text.strip()
@@ -21,18 +19,14 @@ if r.status_code == requests.codes.ok:
     producer = KafkaProducer(bootstrap_servers = broker)
     print('Start sending messages')
 
+    # While not terminated keep getting msgs from provider and sending them to Kafka
     while True:
-    #for i in range(5):
         rates = requests.get(request_body)
         if rates.status_code == requests.codes.ok:
-            #if rates.text.strip() == '':
-            #    print("GET returned NO msgs. Nothing to send to pipeline")
-            #    continue
-            #print("here {}".format(rates.text.strip()))
             jsons = ''
             for rate in rates.text.strip().split("\n"):
                 values = rate.split(",")
-                #print("here {}".format(values))
+                # Check schema of incomming msgs:
                 if len(values) < 9:
                     print("GET returned unexpected msg. Nothing to send to pipeline")
                     continue
@@ -48,5 +42,3 @@ if r.status_code == requests.codes.ok:
                 #log.exception()
                 print('Message failed')
                 pass
-        #print('Message sent')
-        #time.sleep(5)
