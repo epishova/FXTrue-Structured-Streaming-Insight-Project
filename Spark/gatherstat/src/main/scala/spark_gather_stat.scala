1845e378d96cbf3cc1b69824a87a9ead579dd3d9
epishova <34798874+epishova@users.noreply.github.com>
2018-07-07 21:28:30 -0400

    Demo version ready

--- Spark/gatherstat/src/main/scala/spark_gather_stat.scala
+++ Spark/gatherstat/src/main/scala/spark_gather_stat.scala
@@ -16,39 +16,38 @@ import com.datastax.spark.connector._
 
 object SimpleCountStat {
   def main(args: Array[String]) {
-    
+    // Read dates for which to calculate statistics
     if (args.length != 1) {
       println("Usage: '2018-07-05 2018-07-06 2018-07-07'")
       System.exit(1)
     }
     val date_range = args(0).split(" ").toSeq
+    val input_fx_marker = "USD/JPY"
 
     val conf = new SparkConf(true).set("spark.cassandra.connection.host", "ec2-52-23-103-178.compute-1.amazonaws.com")
-    //val sc = new SparkContext("local", "GatherSimpleCountStats", conf)
     val sc = new SparkContext("spark://ec2-18-232-26-53.compute-1.amazonaws.com:7077", "GatherSimpleCountStats", conf)
 
     val sqlContext = new SQLContext(sc)
     import sqlContext.implicits._
     
+    // Connect to Cassandra
     val df = sqlContext
     .read
     .format("org.apache.spark.sql.cassandra")
     .options(Map( "table" -> "fx_rates", "keyspace" -> "fx"))
     .load()
 
-    //df.printSchema
-
-    // val date_range = Seq("2018-07-05", "2018-07-06", "2018-07-07")
-
-    val parsed = df.filter(($"fx_marker" === "USD/JPY") && ($"timestamp_d".isin(date_range: _*)))
+    val parsed = df.filter(($"fx_marker" === input_fx_marker) && ($"timestamp_d".isin(date_range: _*)))
     .select($"timestamp_ms")
-
+    
+    // Compute stats:
     val statdf = parsed.groupBy(window(parsed.col("timestamp_ms"), "10 seconds")) //$"fx_marker", 
     .count()
     .agg(avg($"count").as("avg_10sec"), stddev($"count").as("sbddev_10sec"))
-    .withColumn("fx_marker", lit("USD/JPY"))
+    .withColumn("fx_marker", lit(input_fx_marker))
     .withColumn("stat_gathered_date", lit(from_utc_timestamp(current_timestamp(), "America/New_York")))
 
+    //Write to Cassandra in append mode:
     statdf.write
     .format("org.apache.spark.sql.cassandra")
     .options(Map( "table" -> "anomaly_count_statistics", "keyspace" -> "fx"))
